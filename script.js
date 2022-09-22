f32441ddce6e11e9b79a23d769ff297abb1c7680
Susie-Mars <marsnarsbars@gmail.com>
2022-09-22 12:55:41 -0600

    MakingConnections

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+// CHANGE USERNAME FUNCTION
+function changeText(element) {
+    document.querySelector(element).innerText= "Susie Mars";
+}
+
+// REMOVE REQUESTS FUNCTION
+function hide(element) {
+    document.querySelector(element).remove();
+}
